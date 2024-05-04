5b917c7b67a2ed6db26fe3feaf312975cadacbab
CASTELLI Valerio <valeriocastellipro@gmail.com>
2024-05-05 00:45:48 +0200

    fix: progressbar width

--- src/components/progressbar/ProgressBar.tsx
+++ src/components/progressbar/ProgressBar.tsx
@@ -7,12 +7,12 @@ import { colors } from "@/lib/const"
 export const ProgressBarComponent = ({
     value: number
 }) => {
-    const width = 370;
+    const width = 355;
 
     const getWidthValue = (value: number) => (value/100)*width
     const getPositionRocket = (value: number) => getWidthValue(value)-65
 
-    const getColorProgressBar = (value: number) => {
+    const getColorProgressBar = () => {
         if (number >= 65) return colors.greenColor
         if (number >= 35 && number <= 65) return colors.orangeColor
         if (number <= 35) return colors.primaryColor
@@ -20,7 +20,7 @@ export const ProgressBarComponent = ({
 
     return (
         <View style={styles.container}>
-            <View style={[styles.bar, {width: getWidthValue(number), backgroundColor: getColorProgressBar(number)}]} />
+            <View style={[styles.bar, {width: getWidthValue(number), backgroundColor: getColorProgressBar()}]} />
             <View style={styles.barFull} />
             <Image alt="Logo" resizeMode="contain" source={Rocket} style={[styles.rocket, {left: getPositionRocket(number)}]}/>
         </View>
